aaa6c570f8d92ecb448353e3f646d22b8050b72f
seadgashi <seadgashi01@gmail.com>
2022-08-10 17:24:21 +0200

    Add movieDetail and some general fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Footer from "./Components/footer/Footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./app.css"
+import HomePage from "./Pages/HomePage";
+import MovieDetail from './Components/movieDetail/MovieDetail';
 function App() {
   return (
-    <div className="page-container">
-      <div className="content-wrap">
-    
-      </div>
-      <Footer />
-    </div>
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element= {<HomePage/>} />
+          <Route path="movieDetail" element ={<MovieDetail/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
