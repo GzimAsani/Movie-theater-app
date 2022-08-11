ab00f937697f02ed8000db09a9f7479816d41a0e
Albenoo <albeniskerqeli5@gmail.com>
2022-08-11 16:49:16 +0200

    added lower part of movieDetail

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import HomePage from "./Pages/HomePage";
 import "./app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/navbar/NavBar";
-import Dashboard from "./pages/Dashboard";
-import Concessions from "./pages/Concessions";
+import Dashboard from "./Pages/Dashboard";
+import Concessions from "./Pages/Concessions";
 import Footer from "./Components/footer/Footer";
 import MovieDetail from "./Components/movieDetail/MovieDetail";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/Conccesions" element={<Concessions />} />
-          <Route path="/movieDetail" element={<MovieDetail />} />
-        </Routes>
-      </Navbar>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Conccesions" element={<Concessions />} />
+        <Route path="/movieDetail" element={<MovieDetail />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/data/images.js
@@ -0,0 +1,10 @@
+export const movieImages = [
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2044808/2044808_original.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2043174/2043174_original.jpg",
+  "https://m.media-amazon.com/images/M/MV5BYmI5MjJlZGEtOTRjMS00YWJlLWJjZGEtMmNkODVlM2ZhNDFhXkEyXkFqcGdeQXVyODUxNjcxNjE@._V1_.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2043425/2043425_original.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2043753/2043753_original.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2044646/2044646_original.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2045299/2045299_original.jpg",
+  "https://ic.pics.livejournal.com/seldonp38/50009290/2039690/2039690_original.jpg",
+];
