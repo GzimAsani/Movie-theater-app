b53f305c4c996d1e3c0e8b5f9b1c91ee9c496470
seadgashi <seadgashi01@gmail.com>
2022-08-15 19:55:34 +0200

    movie category filter

--- src/Pages/ConcessionsPages/Header.js
+++ src/Pages/ConcessionsPages/Header.js
@@ -1,25 +1,21 @@
 import React from "react";
 import "./Header.css";
+import "../ConcessionsPages/Show"
 // import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
   return (
+
     <div className="header">
       <h3 className="header-h1">Movie</h3>
-
       <div className="header-mid">
-        {/* <SearchIcon className="header-icon" /> */}
-        <input
-          className="header-input"
-          type="text"
-          placeholder="search the movie"
-        />
+        {/* <SearchIcon className="header-icon" /> */}  
       </div>
       <div className="header-right">
         <h3>Login</h3>
       </div>
     </div>
-  );
+  );    
 }
 
 export default Header;
