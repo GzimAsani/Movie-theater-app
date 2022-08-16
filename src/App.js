4a613fb0f8626790c9b31ab93e1df7296af6f944
Gzim Asani <gzim.asani@gig-ext.com>
2022-08-16 09:47:26 +0200

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from "./Pages/HomePage";
 import "./app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/navbar/NavBar";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./Pages/Dashboard";
 import Movies from "./Pages/Movies";
 import Footer from "./Components/footer/Footer";
 import SignUp from "./Components/SignUp/SignUp";
@@ -19,7 +19,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/Dashboard" element={<Dashboard />} />
-            <Route path="/Conccesions" element={<Movies />} />
+            <Route path="/Movies" element={<Movies />} />
             <Route path="/BookScreen" element={<BookScreen />} />
             <Route path="/MovieDetail" element={<MovieDetail />} />
             <Route path="/SignUp" element={<SignUp />} />
