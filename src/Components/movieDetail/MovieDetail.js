dd6d9ee31fd8dd8948dbca1e422c6fe492f2560b
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2022-08-22 22:58:04 +0000

    link movies to movieDetail component & bug fixes

--- src/Components/movieDetail/MovieDetail.js
+++ src/Components/movieDetail/MovieDetail.js
@@ -1,5 +1,6 @@
-import React from "react";
+import {useEffect,useState} from "react";
 import img from "../../img/tenet.png";
+import {useParams} from 'react-router-dom'
 import Gallery from "./Gallery";
 import { BiMovie } from "react-icons/bi";
 
@@ -7,51 +8,74 @@ import "./movieDetail.css";
 import MovieSchedule from "./ScheduleList";
 
 function MovieDetail() {
+
+  const [movie,setMovie] = useState({});
+  const [loading,setLoading] = useState(false);
+  const {id} = useParams();
+
+  useEffect(() => {
+   const fetchSingleMovie = async() => {
+    setLoading(true);
+    try{
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=b93a64480573ce5248c28b200d79d029&language=en-US`);
+      
+      const movieData = await res.json();
+      setMovie(movieData);
+setLoading(false);
+
+    }
+    catch(err) {
+      console.log(err);
+  }
+   }
+   fetchSingleMovie();
+
+  },[])
+
   return (
-    <div className="movieDetail-contanier">
-      <img src={img} />
-      <div className="movie">
-        <h1>Tenet</h1>
+ 
+    (loading ? <h1>Loading</h1> : <div className="movieDetail-contanier">
+    <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}/>
+    <div className="movie">
+      <h1>{movie.original_title}</h1>
 
-        <h4>
-          A secret agent is given a single word as his weapon and sent to
-          prevent the onset of World War III.
-          <br />
-          He must travel through time and bend the laws of nature in order to be
-          successful in his mission
-        </h4>
+      <h4>
+        {movie.overview}
+      </h4>
 
-        <div className="movie-text">
-          <p>
-            <span className="movie-span">Starts</span> - 01.10.2020{" "}
-          </p>
-          <p>
-            <span className="movie-span">Duration of the film</span> - 150 min{" "}
-          </p>
-          <p>
-            <span className="movie-span">State</span> - USA{" "}
-          </p>
-          <p>
-            <span className="movie-span">Genre</span>-Action, Drama, Thriller
-          </p>
-          <p>
-            <span className="movie-span">Actors </span>-Aaron Taylor-Johnson,
-            Robert Pattinson, Kenneth Branagh, Elizabeth Debicki, John David
-            Washington
-          </p>
-        </div>
+      <div className="movie-text">
+        <p>
+          <span className="movie-span">Starts</span> -{movie.release_date}
+        </p>
+        <p>
+          <span className="movie-span">Duration of the film</span> - {movie.runtime}min
+        </p>
+        <p>
+          <span className="movie-span">State</span> - USA{" "}
+        </p>
+        <p>
+          <span className="movie-span">Genre</span>- {movie?.genres?.map(genre => (
+           <span className="movie-genre" key={genre.id}>{genre.name},</span>
+          ))}
+        </p>
+        <p>
+          <span className="movie-span">Actors </span>-Aaron Taylor-Johnson,
+          Robert Pattinson, Kenneth Branagh, Elizabeth Debicki, John David
+          Washington
+        </p>
+      </div>
 
-        <div className="line"></div>
+      <div className="line"></div>
 
-        <h1>Gallery</h1>
-        <Gallery />
-        <MovieSchedule />
-        <div className="movie-footer">
-          <BiMovie className="movie-footer-icon" />
-          <h1>Online Shopping Reservation</h1>
-        </div>
+      <h1>Gallery</h1>
+      <Gallery />
+      <MovieSchedule />
+      <div className="movie-footer">
+        <BiMovie className="movie-footer-icon" />
+        <h1>Online Shopping Reservation</h1>
       </div>
     </div>
+  </div>)
   );
 }
 export default MovieDetail;
